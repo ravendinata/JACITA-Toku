60a8614d2da5e905d9e20f3dcde58d9520e47468
Raven Limadinata <raven.limadinata@outlook.com>
2024-09-11 09:53:16 +0700

    Add non-validated item database model

--- app/models/items.py
+++ app/models/items.py
@@ -1,5 +1,12 @@
+from sqlalchemy import func
+
 from app.extensions import db
 from app.models.misc import Category, QuantityUnit
+from app.models.user import User
+
+# =====================
+# VALIDATED ITEM MODELS
+# =====================
 
 class Items(db.Model):
     __tablename__ = 'items'
@@ -33,6 +40,29 @@ class ViewItems(db.Model):
 
     def __repr__(self):
         return f'<ViewItems: {self.brand} {self.name} {self.variant} [{self.id}]>'
+    
+    def to_dict(self):
+        return { c.name: getattr(self, c.name) for c in self.__table__.columns }
+    
+# =========================
+# NON-VALIDATED ITEM MODELS
+# =========================
+
+class NonvalItems(db.Model):
+    __tablename__ = 'nonval_items'
+    __table_args__ = { 'schema': 'toku' }
+
+    id = db.Column(db.String, primary_key = True)
+    category_id = db.Column(db.Integer, db.ForeignKey(Category.id), nullable = False, default = 0)
+    brand = db.Column(db.String, nullable = False)
+    name = db.Column(db.String, nullable = False)
+    variant = db.Column(db.String, nullable = True)
+    base_price = db.Column(db.Float, nullable = False)
+    created_date = db.Column(db.DateTime, nullable = False, default = func.current_timestamp())
+    created_by = db.Column(db.String, db.ForeignKey(User.username), nullable = False)
+
+    def __repr__(self):
+        return f'<NonvalItems: {self.brand} {self.name} {self.variant} [{self.id}]>'
     
     def to_dict(self):
         return { c.name: getattr(self, c.name) for c in self.__table__.columns }
