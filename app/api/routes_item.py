c41291fa581bb531ec4a4682128057422ab0bcc5
Raven Limadinata <raven.limadinata@outlook.com>
2024-09-11 09:15:21 +0700

    Implement update and delete item endpoints

--- app/api/routes_item.py
+++ app/api/routes_item.py
@@ -63,3 +63,59 @@ def api_create_item():
         return jsonify({ 'error': 'Error while creating item', 'details': f"{e}" }), 500
 
     return jsonify({ 'message': 'Item created successfully' })
+
+@api.route('/items/validated/<string:item_id>', methods = ['PATCH'])
+def api_update_item(item_id):
+    item = Items.query.get(item_id)
+
+    if item is None:
+        return jsonify({ 'error': 'Item not found' }), 404
+    
+    modifiable_fields = ['brand', 'name', 'variant', 'base_price', 'category_id', 'qty_unit_id']
+    if not any([ request.form.get(field) for field in modifiable_fields ]):
+        return jsonify({ 'error': 'No modifiable fields provided' }), 400
+
+    brand = request.form.get('brand')
+    name = request.form.get('name')
+    variant = request.form.get('variant')
+    base_price = request.form.get('base_price')
+    category_id = request.form.get('category_id')
+    qty_unit_id = request.form.get('qty_unit_id')
+
+    if brand is not None:
+        item.brand = brand
+    if name is not None:
+        item.name = name
+    if variant is not None:
+        item.variant = variant
+    if base_price is not None:
+        item.base_price = base_price
+    if category_id is not None:
+        item.category_id = category_id
+    if qty_unit_id is not None:
+        item.qty_unit_id = qty_unit_id
+
+    try:
+        db.session.commit()
+    except Exception as e:
+        print(f"Error while updating item: {e}")
+        return jsonify({ 'error': 'Error while updating item', 'details': f"{e}" }), 500
+    
+    return jsonify({ 'message': 'Item updated successfully' })
+
+@api.route('/items/validated/<string:item_id>', methods = ['DELETE'])
+def api_delete_item(item_id):
+    item = Items.query.get(item_id)
+
+    if item is None:
+        return jsonify({ 'error': 'Item not found' }), 404
+
+    try:
+        db.session.delete(item)
+        db.session.commit()
+    except Exception as e:
+        print(f"Error while deleting item: {e}")
+        return jsonify({ 'error': 'Error while deleting item', 'details': f"{e}" }), 500
+    
+    return jsonify({ 'message': 'Item deleted successfully' })
+
