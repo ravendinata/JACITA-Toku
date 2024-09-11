9ed593202fd0670fa7ede8e04915cc505b25b5ae
Raven Limadinata <raven.limadinata@outlook.com>
2024-09-11 09:49:35 +0700

    Implement authentication helper

---
+++ helper/auth.py
@@ -0,0 +1,40 @@
+import bcrypt
+
+def generate_password_hash(password):
+    """
+    Generate a password hash from a password.
+
+    Parameters:
+    password : str
+        The password to hash.
+
+    Returns:
+    str
+        The hashed password.
+    """
+
+    password = password.encode('utf-8')
+    salt = bcrypt.gensalt()
+    
+    return bcrypt.hashpw(password, salt).decode('utf-8')
+
+def check_password_hash(hashed_password, raw_password):
+    """
+    Check if a password matches a hashed password.
+
+    Parameters:
+    hashed_password : str
+        The hashed password to check against.
+
+    password : str
+        The password to check.
+
+    Returns:
+    bool
+        True if the password matches the hashed password, False otherwise.
+    """
+
+    hashed_password = hashed_password.encode('utf-8')
+    raw_password = raw_password.encode('utf-8')
+    
+    return bcrypt.checkpw(raw_password, hashed_password)
