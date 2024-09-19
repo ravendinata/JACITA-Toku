d1676bd2ff236da58c0d4e61fba34c55660e5bf1
Raven Limadinata <raven.limadinata@outlook.com>
2024-09-19 14:50:52 +0700

    Implement add item page

--- app/web/routes_item.py
+++ app/web/routes_item.py
@@ -1,6 +1,7 @@
 from flask import render_template, session, redirect, url_for
 
 from app.web import web
+from app.models.misc import Category, QuantityUnit
 from helper.auth import is_authenticated
 
 @web.route('/items')
@@ -17,4 +18,13 @@ def page_items_add():
         return redirect(url_for('web.page_login'))
     else:
         data = { 'username': session['user'] }
-        return render_template('items/add.html', title = "Add Item", data = data)
+        
+        units = QuantityUnit.query.all()
+        units = [ unit.to_dict() for unit in units ]
+
+        categories = Category.query.all()
+        categories = [ category.to_dict() for category in categories ]
+        categories = sorted(categories, key = lambda x: x['id'])
+
+        return render_template('items/add.html', title = "Add Item", 
+                               data = data, units = units, categories = categories)
