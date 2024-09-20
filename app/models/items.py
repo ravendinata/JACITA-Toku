cb4158fee083a5ad18d4fbdaf76ed5f8b638dee3
Raven Limadinata <raven.limadinata@outlook.com>
2024-09-20 10:25:51 +0700

    Update items database model
    - Added description field
    - Added modification metadata fields
    - Fixed default value for nonval category ID

--- app/models/items.py
+++ app/models/items.py
@@ -21,6 +21,9 @@ class Items(db.Model):
     qty_unit_id = db.Column(db.Integer, db.ForeignKey(QuantityUnit.id), nullable = False, default = 0)
     created_date = db.Column(db.DateTime, nullable = False, default = func.current_timestamp())
     created_by = db.Column(db.String, db.ForeignKey(User.username), nullable = False)
+    description = db.Column(db.String, nullable = True)
+    modification_date = db.Column(db.DateTime, nullable = True, onupdate = func.current_timestamp())
+    modification_by = db.Column(db.String, db.ForeignKey(User.username), nullable = True)
 
     def __repr__(self):
         return f'<Items: {self.brand} {self.name} {self.variant} [{self.id}]>'
@@ -41,6 +44,8 @@ class ViewItems(db.Model):
     qty_unit = db.Column(db.String, nullable = False, default = 0)
     created_date = db.Column(db.DateTime, nullable = False, default = func.current_timestamp())
     created_by = db.Column(db.String, db.ForeignKey(User.username), nullable = False)
+    modification_date = db.Column(db.DateTime, nullable = True)
+    modification_by = db.Column(db.String, db.ForeignKey(User.username), nullable = True)
 
     def __repr__(self):
         return f'<ViewItems: {self.brand} {self.name} {self.variant} [{self.id}]>'
@@ -57,13 +62,16 @@ class NonvalItems(db.Model):
     __table_args__ = { 'schema': 'toku' }
 
     id = db.Column(db.String, primary_key = True)
-    category_id = db.Column(db.Integer, db.ForeignKey(Category.id), nullable = False, default = 0)
+    category_id = db.Column(db.Integer, db.ForeignKey(Category.id), nullable = False, default = 99)
     brand = db.Column(db.String, nullable = False)
     name = db.Column(db.String, nullable = False)
     variant = db.Column(db.String, nullable = True)
     base_price = db.Column(db.Float, nullable = False)
     created_date = db.Column(db.DateTime, nullable = False, default = func.current_timestamp())
     created_by = db.Column(db.String, db.ForeignKey(User.username), nullable = False)
+    description = db.Column(db.String, nullable = True)
+    modification_date = db.Column(db.DateTime, nullable = True, onupdate = func.current_timestamp())
+    modification_by = db.Column(db.String, db.ForeignKey(User.username), nullable = True)
 
     def __repr__(self):
         return f'<NonvalItems: {self.brand} {self.name} {self.variant} [{self.id}]>'
@@ -76,13 +84,15 @@ class ViewNonvalItems(db.Model):
     __table_args__ = { 'schema': 'toku' }
 
     id = db.Column(db.String, primary_key = True)
-    category = db.Column(db.String, nullable = False, default = 0)
+    category = db.Column(db.String, nullable = False, default = 99)
     brand = db.Column(db.String, nullable = False)
     name = db.Column(db.String, nullable = False)
     variant = db.Column(db.String, nullable = True)
     base_price = db.Column(db.Float, nullable = False)
     created_date = db.Column(db.DateTime, nullable = False, default = func.current_timestamp())
     created_by = db.Column(db.String, nullable = False)
+    modification_date = db.Column(db.DateTime, nullable = True)
+    modification_by = db.Column(db.String, nullable = True)
 
     def __repr__(self):
         return f'<ViewNonvalItems: {self.brand} {self.name} {self.variant} [{self.id}]>'
