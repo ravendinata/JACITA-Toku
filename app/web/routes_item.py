313bd35fcc3b0b2949f01870e6645a585d3b0ff0
Raven Limadinata <raven.limadinata@outlook.com>
2024-09-23 12:22:53 +0700

    Implement permissions on item processing pages
    - Update the logic of the page
    - Update server routes logic
    - Implement new endpoint decorators

--- app/web/routes_item.py
+++ app/web/routes_item.py
@@ -3,59 +3,62 @@ from flask import render_template, session, redirect, url_for
 from app.web import web
 from app.models.items import Items, NonvalItems
 from app.models.misc import Category, QuantityUnit
-from helper.auth import is_authenticated
+from app.models.user import User
+from helper.auth import check_login
+from helper.endpoint import inject_allowed_operations, check_page_permission, check_page_permissions
 
 @web.route('/items')
-def page_items_view_all():
-    if not is_authenticated(session):
-        return redirect(url_for('web.page_login'))
-    else:
-        data = { 'username': session['user'] }
-        return render_template('items/view_all.html', title = "Items", data = data, use_datatables = True)
+@check_login
+@inject_allowed_operations
+def page_items_view_all(user_operations):
+    data = { 'username': session['user'] }
+    return render_template('items/view_all.html', use_datatables = True, title = "Items", data = data, operations = user_operations)
     
 @web.route('/items/add')
-def page_items_add():
-    if not is_authenticated(session):
-        return redirect(url_for('web.page_login'))
-    else:
-        data = { 'username': session['user'] }
-        
-        units = QuantityUnit.query.all()
-        units = [ unit.to_dict() for unit in units ]
+@check_login
+@inject_allowed_operations
+@check_page_permissions([ 'item_validated/create', 'item_nonvalidated/create' ])
+def page_items_add(user_operations):
+    data = { 'username': session['user'] }
+    
+    units = QuantityUnit.query.all()
+    units = [ unit.to_dict() for unit in units ]
 
-        categories = Category.query.all()
-        categories = [ category.to_dict() for category in categories ]
-        categories = sorted(categories, key = lambda x: x['id'])
+    categories = Category.query.all()
+    categories = [ category.to_dict() for category in categories ]
+    categories = sorted(categories, key = lambda x: x['id'])
 
-        return render_template('items/add.html', title = "Add Item", 
-                               data = data, units = units, categories = categories)
+    return render_template('items/add.html', title = "Add Item", 
+                            data = data, units = units, categories = categories, operations = user_operations)
     
 @web.route('/item/<string:id>/edit')
+@check_login
 def page_items_edit(id):
-    if not is_authenticated(session):
-        return redirect(url_for('web.page_login'))
-    else:
-        data = { 'username': session['user'] }
-        
-        units = QuantityUnit.query.all()
-        units = [ unit.to_dict() for unit in units ]
+    username = session['user']
 
-        categories = Category.query.all()
-        categories = [ category.to_dict() for category in categories ]
-        categories = sorted(categories, key = lambda x: x['id'])
+    data = { 'username': username }
+    
+    units = QuantityUnit.query.all()
+    units = [ unit.to_dict() for unit in units ]
 
-        item = Items.query.get(id)
-        if item is None:
-            item = NonvalItems.query.get(id)
-            if item is None:
-                return redirect(url_for('web.page_items_view_all'))
-            else:
-                item_type = 'nonvalidated'
+    categories = Category.query.all()
+    categories = [ category.to_dict() for category in categories ]
+    categories = sorted(categories, key = lambda x: x['id'])
+
+    item = Items.query.get(id) or NonvalItems.query.get(id)
+    if item is None:
+        return redirect(url_for('web.page_items_view_all'))
+
+    item_type = 'validated' if isinstance(item, Items) else 'nonvalidated'
+
+    @check_page_permission(f"item_{item_type}/update")
+    def page():
+        user = User.query.get(username)
+        if item.created_by != username and not user.can_update_items():
+            return render_template('error/standard.html', title = "Forbidden", code = 403, message = "You are not the creator of this item.", data = data), 403
         else:
-            item_type = 'validated'
+            data['item_type'] = item_type
+            return render_template('items/edit.html', title = "Edit Item", 
+                                    data = data, units = units, categories = categories, item = item.to_dict())
 
-        item = item.to_dict()
-        data['item_type'] = item_type
-
-        return render_template('items/edit.html', title = "Edit Item", 
-                               data = data, units = units, categories = categories, item = item)
+    return page()
--- helper/endpoint.py
+++ helper/endpoint.py
@@ -1,3 +1,10 @@
+from functools import wraps
+
+from flask import current_app, render_template, session
+
+from app.models.user import User
+from helper.role import InsufficientPermissionError
+
 class HTTPStatus:
     OK = 200
     CREATED = 201
@@ -46,3 +53,44 @@ def check_fields(request, endpoint):
             return { 'pass': False, 'error': 'No modifiable fields provided', 'available_fields': modifiable }
 
     return { 'pass': True }
+
+def check_page_permission(permission: str):
+    def decorator(func):
+        @wraps(func)
+        def wrapper(*args, **kwargs):
+            user = User.query.get(session['user'])
+            try:
+                user.can_do(permission)
+            except InsufficientPermissionError as e:
+                return render_template('error/standard.html', title = "Forbidden", code = 403, message = e.message, data = { 'username': session['user'] }), HTTPStatus.FORBIDDEN
+            return func(*args, **kwargs)
+        return wrapper
+    return decorator
+
+def check_page_permissions(permissions: list):
+    def decorator(func):
+        @wraps(func)
+        def wrapper(*args, **kwargs):
+            user = User.query.get(session['user'])
+            try:
+                for permission in permissions:
+                    try:
+                        if user.can_do(permission):
+                            return func(*args, **kwargs)
+                    except InsufficientPermissionError:
+                        continue
+                raise InsufficientPermissionError(user, permissions)
+            except InsufficientPermissionError as e:
+                return render_template('error/standard.html', title = "Forbidden", code = 403, message = e.message, data = { 'username': session['user'] }), HTTPStatus.FORBIDDEN
+        return wrapper
+    return decorator
+
+def inject_allowed_operations(func):
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        user = User.query.get(session['user'])
+        operations = user.allowed_operations()
+        
+        return func(*args, user_operations = operations, **kwargs)
+    
+    return wrapper
