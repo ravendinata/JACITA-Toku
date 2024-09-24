8d257b6f9ba5faf8ae03670245ccc9e760a17b15
Raven Limadinata <raven.limadinata@outlook.com>
2024-09-24 13:26:33 +0700

    Implement view order details page
    - Created server routes file
    - Created the page itself

---
+++ app/web/routes_order.py
@@ -0,0 +1,14 @@
+from flask import render_template, session
+
+from app.web import web
+from app.models.orders import Orders
+from helper.auth import check_login
+
+@web.route('/order/<string:id>')
+@check_login
+def page_order_view(id):
+    order = Orders.query.get(id)
+    if order is None:
+        return render_template('error/standard.html', title = "Not Found", code = 404, message = "Order not found."), 404
+
+    return render_template('orders/detail.html', use_datatables = True, title = "View Order", order = order, data = { 'username': session['user'] })
