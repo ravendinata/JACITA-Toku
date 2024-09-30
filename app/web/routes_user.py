9c9f903aa5e4c9d9e5f0d2877ecfb0e1e7a7ae0a
Raven Limadinata <raven.limadinata@outlook.com>
2024-09-30 14:53:46 +0700

    Implement user administration pages

--- app/web/routes_user.py
+++ app/web/routes_user.py
@@ -1,8 +1,10 @@
 from flask import render_template, session, redirect, url_for
 
 from app.web import web
-from app.models.user import User
-from helper.auth import is_authenticated
+from app.models.user import User, Division
+from helper.auth import is_authenticated, check_login
+from helper.endpoint import check_page_permission
+from helper.role import Role
 
 @web.route('/login')
 def page_login():
@@ -12,10 +14,23 @@ def page_login():
         return render_template('user/login.html', title = "Login", next_url = session.get('next'))
     
 @web.route('/user/profile')
+@check_login
 def page_profile():
-    if is_authenticated(session):
-        username = session.get('user')
-        user = User.query.get(username)
-        return render_template('user/profile.html', title = "Profile", user = user.to_dict())
-    else:
-        return redirect(url_for('web.login'))
+    username = session.get('user')
+    user = User.query.get(username)
+    return render_template('user/profile.html', title = "Profile", user = user.to_dict())
+
+@web.route('/user/<string:username>')
+@check_login
+@check_page_permission('user/administer')
+def page_user(username):
+    user = User.query.get(username)
+    return render_template('user/profile.html', title = "User", user = user.to_dict(), admin = True)
+
+@web.route('/users')
+@check_login
+@check_page_permission('user/administer')
+def page_users():
+    division = Division.query.all()
+    roles = Role.get_roles()
+    return render_template('user/view_all.html', use_datatables = True, title = "Users", roles = roles, divisions = division)
