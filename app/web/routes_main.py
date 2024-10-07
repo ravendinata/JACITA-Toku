f1b59b32dcf95de053e459a5bf9c04e0899ba823
Raven Limadinata <raven.limadinata@outlook.com>
2024-10-07 10:05:48 +0700

    Redesign home page

--- app/web/routes_main.py
+++ app/web/routes_main.py
@@ -1,4 +1,6 @@
-from flask import render_template, session, redirect, url_for
+from datetime import datetime
+
+from flask import render_template
 
 from app.web import web
 from helper.auth import check_login
@@ -6,4 +8,7 @@ from helper.auth import check_login
 @web.route('/')
 @check_login
 def index():
-    return render_template('index.html', title = "Home")
+    curr_period_short = datetime.now().strftime('%Y/%m')
+    curr_period_long = datetime.now().strftime('%B %Y')
+    current_period = { 'short': curr_period_short, 'long': curr_period_long }
+    return render_template('index.html', title = "Home", current_period = current_period)
