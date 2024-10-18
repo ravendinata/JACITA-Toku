67fac97f13139ee8ffda98aa8082a65cbb42492c
Raven Limadinata <raven.limadinata@outlook.com>
2024-10-18 14:43:04 +0700

    Replace navbar with user on context processor

--- app/context_processor.py
+++ app/context_processor.py
@@ -9,18 +9,17 @@ def inject_session_data():
     session_data_keys = [ 'user', 'active_order' ]
     data = {}
     navbar = []
+    user = None
 
     if 'user' in session:
+        user = User.query.get(session['user'])
         active_order = Orders.query.filter(~Orders.status.in_([10, 99]), Orders.created_by == session['user']).order_by(desc(Orders.created_date)).first()
         if active_order:
             session['active_order'] = active_order.id
         else:
             session.pop('active_order', None)
-
-        role = User.query.get(session['user']).role
-        navbar = get_navbar_items(role) if 'user' in session else []
     
     for key in session_data_keys:
         data[key] = session.get(key, None)
 
-    return { 'data': data, 'navbar': navbar }
+    return { 'data': data, 'user': user }
