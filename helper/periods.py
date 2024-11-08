9ffa611d33c86d2425dd5f4708cbfc06a046acf3
Raven Limadinata <raven.limadinata@outlook.com>
2024-11-08 13:44:14 +0700

    Parameterize length of available periods getter

--- helper/periods.py
+++ helper/periods.py
@@ -13,7 +13,7 @@ def get_next_month():
 def get_current_period():
     return get_available_periods()[0]
 
-def get_available_periods():
+def get_available_periods(length = 3):
     today = datetime.now()
     
     if today.day <= 25:
@@ -24,7 +24,7 @@ def get_available_periods():
     periods = []
     
     # Generate the next 3 periods
-    for i in range(3):
+    for i in range(length):
         target_month = first_period + relativedelta(months = i)
         order_start = (target_month + relativedelta(months = -2, day = 25))
         order_end = (target_month + relativedelta(months = -1, day = 25))
